a708e27114f7f60eb33ec62d382f3d92a5fb6a4f
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-17 14:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let count=0;
 let btn= document.getElementById("incrementBtn");
 btn.addEventListener("click",()=>{
-    alert("Un-incremented value:" + count);
+    alert(count);
     count++;
     document.getElementById("counter").innerText=count;
 })
